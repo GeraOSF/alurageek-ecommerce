2fc1f3df75d92f0a5a71eee42df784ea57142837
Gerardo Gómez <gerardogomezromero175@gmail.com>
2022-08-05 00:07:47 -0700

    Simplified admin-login script

--- js/admin-login.js
+++ js/admin-login.js
@@ -8,20 +8,17 @@ const notAdminErrMessage = document.querySelector(".not-admin__error-message");
 inputEmail.addEventListener("blur", validateInput);
 inputPassword.addEventListener("blur", validateInput);
 
-async function getUsers() {
-  return await fetch("http://localhost:3000/users")
-    .then(res => res.json());
-}
-
 loginForm.addEventListener("submit", e => {
   e.preventDefault();
   // Fetch users and check if the user is in the database (is an admin)
-  getUsers().then(users => {
-    for (let i = 0; i < users.length; i++) {
-      if (users[i].email == inputEmail.value && users[i].password == inputPassword.value) {
-        window.location.href = "../pages/admin-menu.html";
+  fetch("http://localhost:3000/users")
+    .then(res => res.json())
+    .then(users => {
+      for (let i = 0; i < users.length; i++) {
+        if (users[i].email == inputEmail.value && users[i].password == inputPassword.value) {
+          window.location.href = "../pages/admin-menu.html";
+        }
       }
-    }
-    notAdminErrMessage.style.visibility = "initial";
-  });
+      notAdminErrMessage.style.visibility = "initial";
+    });
 });
