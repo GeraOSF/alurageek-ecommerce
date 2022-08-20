fdee2690602bcc9dea237f7158b86843fbe3c061
Gerardo Gómez <gerardogomezromero175@gmail.com>
2022-08-20 00:49:55 -0700

    Add view product page

--- js/make-element.js
+++ js/make-element.js
@@ -1,16 +1,14 @@
-function goToViewAllPage(e) {
-  const category = e.target.id;
-  window.location.href = `../pages/view-all.html?category=${category}`;
-}
-
 function productHeader(category) {
   const header = document.createElement("div");
   header.classList.add("product__header");
   header.innerHTML = `<h2 class="product__type">${category}</h2>
-                      <a href="#" id="${category}" class="view-all">View all <i class="fa-solid fa-arrow-right-long"></i></a>`;
+  <a href="#" class="view-all">View all <i class="fa-solid fa-arrow-right-long"></i></a>`;
+  if (category === "Consoles") header.querySelector(".product__type").id = "consoles";
+
   // Add event listener to view all button
   const viewAllBtn = header.querySelector(".view-all");
   viewAllBtn.addEventListener("click", goToViewAllPage);
+
   return header;
 }
 
@@ -21,15 +19,33 @@ function productListItem(image, name, price) {
   const item = document.createElement("li");
   item.classList.add("product__list__element");
   item.innerHTML = `<img src="${image}" alt="Product image" class="product__list__element--image">
-                      <p class="product__name">${name}</p>
-                      <p class="product__price">$ ${price}</p>
+                    <p class="product__name">${name}</p>
+                    <p class="product__price">$ ${price}</p>
                     <div>
                       <a href="#" class="product__view-product">View product</a>
                     </div>`
+
+  // Add event listener to view product button
+  const viewProductBtn = item.querySelector(".product__view-product");
+  viewProductBtn.addEventListener("click", goToViewProductPage);
+  
   return item;
 }
 
 export const makeElement = {
   productHeader,
   productListItem
+}
+
+// Event listener functions
+function goToViewAllPage(e) {
+  const header = e.target.parentNode;
+  const category = header.querySelector(".product__type").textContent;
+  window.location.href = `../pages/view-all.html?category=${category}`;
+}
+
+function goToViewProductPage(e) {
+  const item = e.target.parentNode.parentNode;
+  const productName = item.querySelector(".product__name").textContent;
+  window.location.href = `../pages/view-product.html?name=${productName}`;
 }
